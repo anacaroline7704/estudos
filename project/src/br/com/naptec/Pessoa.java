d87f299390da72bb0a6b9bf896ac82a5c344d14f
nap tec-7 <nap tec-7@DESKTOP-FT05DB9.localdomain>
2018-10-03 21:38:38 -0300

    .

---
+++ project/src/br/com/naptec/Pessoa.java
@@ -0,0 +1,23 @@
+package br.com.naptec;
+
+public class Pessoa {
+
+	private String nome;
+	private Endereco endereco;
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
--- project/src/br/com/naptec/view/Tela.java
+++ project/src/br/com/naptec/view/Tela.java
@@ -1,16 +1,32 @@
 package br.com.naptec.view;
 
 import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 import br.com.naptec.Calculadora;
 
 public class Tela {
 
+	public JTextField getCampo1() {
+		JTextField tx = new JTextField() ;
+		tx.setSize(200, 24);
+		return tx;
+	}
+	
+	public JTextField getCampo2() {
+		JTextField tf = new JTextField() ;
+		tf.setSize(200, 20);
+		return tf;
+	}
+	
 	public JButton botao() {
 		JButton jb = new JButton();
 		jb.setText("botao");
@@ -20,7 +36,13 @@ public class Tela {
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println();
 				Calculadora calc = new Calculadora();
-				calc.divide();
+				
+				String text = getCampo1().getText();
+				String text2 = getCampo2().getText();
+				
+				
+				Integer valorInteiro = Integer.valueOf(text);
+				calc.divide(valorInteiro, Integer.valueOf(text2));
 			}
 		});
 		return jb;
@@ -31,15 +53,20 @@ public class Tela {
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setTitle("Teste");
-		jf.setSize(1024, 650);
+		jf.setSize(1024, 250);
 
-		jf.setLayout(new BorderLayout());
+		jf.setLayout(new GridLayout(4,2));
 
-		jf.getContentPane().add(botao(), BorderLayout.NORTH);
+		jf.getContentPane().add(getCampo1());
+		jf.getContentPane().add(getCampo2());
+		jf.getContentPane().add(botao());
+		jf.getContentPane().setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		jf.pack();
 	}
 
 	public static void main(String[] args) {
 		Tela t = new Tela();
 		t.init();
+		
 	}
 }
